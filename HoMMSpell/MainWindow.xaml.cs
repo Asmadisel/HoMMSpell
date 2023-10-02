78dad5207ab82cdac41f438aca27294ddaca0eb9
Dmitry Ivanov <diemon_2000@mail.ru>
2023-10-02 17:03:46 +0300

    Добавьте файлы проекта.

---
+++ HoMMSpell/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace HoMMSpell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class ViewModel
+    {
+            public ISeries[] Series { get; set; }
+                = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+            };
+    }
+}
